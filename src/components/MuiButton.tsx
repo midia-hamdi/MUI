7d73371287cd5f2082e546c88b21597cc5a230d0
midia-hamdi <midiahamdi367@gmail.com>
2023-12-24 17:06:42 +0330

    Button part Mui

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,4 +1,5 @@
-import {Stack, Button} from '@mui/material'
+import {Stack, Button, IconButton} from '@mui/material'
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 
 export const MuiButton = () => {
   return (
@@ -21,6 +22,21 @@ export const MuiButton = () => {
             <Button variant='contained' size='medium'>medium</Button>
             <Button variant='contained' size='large'>large</Button>
         </Stack>
+        <Stack spacing={2} direction='row'>
+        <Button variant='contained' startIcon={<AccessTimeFilledIcon/>} disableElevation onClick={() => alert('clickes')}>
+          Clock
+        </Button>
+        <Button variant='text' startIcon={<AccessTimeFilledIcon/>} disableRipple>
+          Clock
+        </Button>
+        <Button variant='outlined' startIcon={<AccessTimeFilledIcon/>} >
+          Clock
+        </Button>
+        <IconButton area-aria-label='Clock' color='info' size='small'>
+          <AccessTimeFilledIcon />
+        </IconButton>
+
+    </Stack>
 
     </Stack>
   )
